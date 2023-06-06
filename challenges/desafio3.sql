fac8f36e9dfffaf826d796237c88fda356cdd4f8
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-06 18:16:21 -0300

    feat total_minutos

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,5 +1,10 @@
-SELECT DISTINCT Us.`name` as pessoa_usuaria
+SELECT Us.`name` as pessoa_usuaria,
+COUNT(Hp.user_id) as musicas_ouvidas,
+SUM(Mu.duration_music) / 60 as total_minutos
 FROM SpotifyClone.history_plays AS Hp
+INNER JOIN SpotifyClone.musics AS Mu
+ON Mu.music_id = Hp.music_id
 INNER JOIN SpotifyClone.`user` AS Us
 ON Us.user_id = Hp.user_id
+GROUP BY Us.`name`
 ORDER BY Us.`name`;
