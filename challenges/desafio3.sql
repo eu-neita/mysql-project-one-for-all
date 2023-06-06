9368a53d0b40c3f2a82df9af551338354f72f12d
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-06 18:30:48 -0300

    fix volta as duas casas

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,6 +1,6 @@
 SELECT Us.`name` as pessoa_usuaria,
 COUNT(Hp.user_id) as musicas_ouvidas,
-SUM(Mu.duration_music) / 60 as total_minutos
+ROUND(SUM(Mu.duration_music) / 60, 2) as total_minutos
 FROM SpotifyClone.history_plays AS Hp
 INNER JOIN SpotifyClone.musics AS Mu
 ON Mu.music_id = Hp.music_id
