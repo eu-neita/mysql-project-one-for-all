cadccab043018e88b7e1cb6b5889b90b11c472b4
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-06 19:58:37 -0300

    feat filtra grupo de reproducoes

--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -4,4 +4,9 @@ FROM SpotifyClone.history_plays AS Hp
 INNER JOIN SpotifyClone.`musics` AS Mu
 ON Mu.music_id = Hp.music_id
 GROUP BY Mu.`music_name`
+HAVING COUNT(Hp.music_id) = 
+(SELECT MAX(reproducoes)
+FROM (SELECT COUNT(Hp.music_id)
+AS reproducoes FROM SpotifyClone.history_plays
+AS Hp GROUP BY Hp.music_id) as subquery)
 ORDER BY Mu.`music_name`;
