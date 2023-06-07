1836c030a01b1751d6329ba81176c067abacbad3
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-07 07:47:46 -0300

    fix torna os numeros strings e utiliza format e round para o seguinte formato 0.00

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,6 +1,6 @@
-SELECT MIN(Pl.`value`) AS faturamento_minimo,
-MAX(Pl.`value`) AS faturamento_maximo,
-AVG(Pl.`value`) AS faturamento_medio,
-SUM(Pl.`value`) AS faturamento_total FROM SpotifyClone.plans AS Pl
+SELECT CONVERT(FORMAT(MIN(Pl.`value`), 2), CHAR) AS faturamento_minimo,
+CONVERT(ROUND(MAX(Pl.`value`), 2), CHAR) AS faturamento_maximo,
+CONVERT(ROUND(AVG(Pl.`value`), 2), CHAR) AS faturamento_medio,
+CONVERT(ROUND(SUM(Pl.`value`), 2), CHAR) AS faturamento_total FROM SpotifyClone.plans AS Pl
 INNER JOIN SpotifyClone.user AS Us
 ON Us.plan_id = Pl.plan_id;
