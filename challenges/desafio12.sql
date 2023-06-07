52d9fbbe26853f06d31a3744b7c8edbf1962bd00
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-07 16:56:10 -0300

    fix alias

--- challenges/desafio12.sql
+++ challenges/desafio12.sql
@@ -4,10 +4,10 @@ SELECT Ar.name_artist AS album,
 		WHEN COUNT(Uf.music_id) BETWEEN 3 AND 4 THEN 'B'
 		WHEN COUNT(Uf.music_id) BETWEEN 1 AND 2 THEN 'C'
         ELSE '-'
-	END AS favoritadas
+	END AS ranking
 FROM SpotifyClone.artists AS Ar
 LEFT JOIN SpotifyClone.musics AS Mu ON Ar.artist_id = Mu.artist_id
 LEFT JOIN SpotifyClone.albums AS Al ON Al.album_id = Mu.album_id
 LEFT JOIN SpotifyClone.user_fav_musics AS Uf ON Mu.music_id = Uf.music_id
 GROUP BY Ar.name_artist
-ORDER BY (CASE favoritadas WHEN '-' THEN 1 ELSE 0 END), favoritadas, Ar.name_artist;
+ORDER BY (CASE ranking WHEN '-' THEN 1 ELSE 0 END), ranking, Ar.name_artist;
